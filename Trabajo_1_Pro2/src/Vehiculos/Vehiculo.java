1016b906ec77d867f631f2dc32110a9dbbe8f613
IanOlmedo <i.olmedo@alumno.um.edu.ar>
2025-04-03 15:34:30 -0300

    Clase Vehiculo + Main

---
+++ Trabajo_1_Pro2/src/Vehiculos/Vehiculo.java
@@ -0,0 +1,56 @@
+package Trabajo_1_Pro2.src.Vehiculos;
+
+public class Vehiculo {
+    private String patente;
+    private String marca;
+    private int anio;
+    private double capacidadCargaKg;
+
+    public Vehiculo(String patente, String marca, int anio, double capacidadCargaKg) {
+        this.patente = patente;
+        this.marca = marca;
+        this.anio = anio;
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public double getCapacidadCargaKg() {
+        return capacidadCargaKg;
+    }
+
+    public void setCapacidadCargaKg(double capacidadCargaKg) {
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Marca: " + this.getMarca() +
+                ", Patente: " + this.getPatente() +
+                ", Año: " + this.getAnio() +
+                ", Capacidad de Carga: " + this.getCapacidadCargaKg() + " kg");
+    }
+
+
+}
